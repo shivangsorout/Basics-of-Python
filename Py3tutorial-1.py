c3e186171d6637da3af8406d47f7d6ebe3ad5a31
Shivang Sorout <55359985+shivangsorout@users.noreply.github.com>
2020-10-30 21:20:59 +0530

    Update Py3tutorial-1.py

--- Py3tutorial-1.py
+++ Py3tutorial-1.py
@@ -1,4 +1,8 @@
+def do_something():
+  print("I can do many things")
+  
 print("Hello World")
 print("I am learning git and github")
 print("This is my first commit.")
 print("This is my second commit")
+do_something()
