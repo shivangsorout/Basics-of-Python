83e637173383a8b549b2e702d06ed2b4a8e0df65
ShivangSorout <andyinalldeep@gmail.com>
2020-11-07 18:57:20 +0530

    Resolved an error

--- Argparsing.py
+++ Argparsing.py
@@ -2,6 +2,18 @@ import argparse
 import sys
 import math
 
+		
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x',type = float, default = 1,
+                        help = "What is the value of X?")
+    parser.add_argument('--n',type = float, default = 1,
+                        help = "What is the value of Y?")
+    parser.add_argument('--operation',type = str, default = 'exp',
+                        help = "What operation do you want to perform?")
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
+
 def calc(args):
 	if args.operation == 'exp':
 		return args.x**args.n
@@ -11,17 +23,6 @@ def calc(args):
 		return ((args.x**2)-(args.n**2))
 	elif args.operation == 'exps':
 		return ((args.x**2)+(args.n**2))
-		
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--x',type = float, default = 1,
-                        help = "What is the value of X?")
-    parser.add_argument('--n',type = float, default = 1,
-                        help = "What is the value of Y?")
-    parser.add_argument('--operation',type = str, default = 1,
-                        help = "What operation do you want to perform?")
-    args = parser.parse_args()
-    sys.stdout.write(str(calc(args)))
 
 if __name__ == '__main__':
     main()
