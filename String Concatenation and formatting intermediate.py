4bd84f1c8bcce1d38fdb69908235ef19e2bfcdfd
ShivangSorout <andyinalldeep@gmail.com>
2020-11-07 15:33:03 +0530

    More impactful example of using .join

--- String Concatenation and formatting intermediate.py
+++ String Concatenation and formatting intermediate.py
@@ -5,3 +5,7 @@ names = ['Joe','John','Kite','Phil']
 for name in names:
     statement = " ".join(['Hello there',name])
     print(statement)
+
+#more impactful example for this is if we have to print all the list of the names then
+
+print(', '.join(names))
