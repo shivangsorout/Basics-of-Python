dba9c44e6f6d261d4c523599abc9925971304017
ShivangSorout <andyinalldeep@gmail.com>
2020-11-07 18:50:57 +0530

    Argparsing Completed

--- Argparsing.py
+++ Argparsing.py
@@ -2,15 +2,26 @@ import argparse
 import sys
 import math
 
-def calc(x,n,op):
-	if op == 'exp':
-		return x**n
-	elif op == 'log':
-		return math.log(x,n)
-	elif op == 'expm':
-		return ((x**2)-(n**2))
-	elif op == 'exps':
-		return ((x**2)+(n**2))
+def calc(args):
+	if args.operation == 'exp':
+		return args.x**args.n
+	elif args.operation == 'log':
+		return math.log(args.x,args.n)
+	elif args.operation == 'expm':
+		return ((args.x**2)-(args.n**2))
+	elif args.operation == 'exps':
+		return ((args.x**2)+(args.n**2))
 		
-oper = calc(9,3,'exps')
-print(oper)	
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x',type = float, default = 1,
+                        help = "What is the value of X?")
+    parser.add_argument('--n',type = float, default = 1,
+                        help = "What is the value of Y?")
+    parser.add_argument('--operation',type = float, default = 1,
+                        help = "What operation do you want to perform?")
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
+
+if __name__ == '__main__':
+    main()
