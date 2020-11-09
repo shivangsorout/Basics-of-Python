575dc128d8d53d69a4c83be62929b9fb5674b7d2
ShivangSorout <andyinalldeep@gmail.com>
2020-11-09 23:18:54 +0530

    Used Enumerate for the first time

--- Enumerate.py
+++ Enumerate.py
@@ -3,3 +3,6 @@
 example = ["left" , "right", "up" , "down"]
 for i in range(len(example)):
 	print(i, example[i])
+
+for i,j in enumerate(example):					#this is how we use the enumerate
+	print(i,j)
