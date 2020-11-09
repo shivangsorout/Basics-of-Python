bc807902532259ffb3414a16b824f9eaa268e3e5
ShivangSorout <andyinalldeep@gmail.com>
2020-11-09 23:55:33 +0530

    Completed with the zip function

--- Zip.py
+++ Zip.py
@@ -4,6 +4,7 @@ y = [5,65,3,5,3,2]
 
 z = [9,7,6,4,3,2]
 
+'''
 for i,j in zip(x,y):
     print(i,j)
 
@@ -16,3 +17,26 @@ for i,j,k in zip(x,y,z):
 
 
 print(zip(x,y,z))               #showing that zip fuction creates a zip object
+
+print(list(zip(x,y,z)))         #we can convert zip into list
+
+name = ['Jenny', 'Lilly', 'Peter', 'John']
+num = [99,98,79,60]
+
+rec = dict(zip(name,num))                   #we can also convert a zip into a dictionary
+
+print(rec)                                  
+
+[print(l,k,j) for l,k,j in zip(x,y,z)]      #we can also use list comprehension
+'''
+
+# issue you might face when you were using bad practices
+
+[print(x,y,z) for x,y,z in zip(x,y,z)]      
+
+print(x)                                    #at this time the value of list x doesn't get affected due to list comprehension
+
+
+for x,y,z in zip(x,y,z):
+    print(x,y,z)
+print(x)                                    #but here when we use normal for loop then the value of originals get changed
