8b79cf8cdb4c7268eda4d316301f0a108f7d9859
ShivangSorout <andyinalldeep@gmail.com>
2020-11-09 23:29:32 +0530

    Thats all for the enumerate

--- Enumerate.py
+++ Enumerate.py
@@ -6,3 +6,10 @@ for i in range(len(example)):
 
 for i,j in enumerate(example):					#this is how we use the enumerate
 	print(i,j)
+
+example_dict = {'left':'<','right':'>','up':'^','down':'v',} 	#we use this '{}' for creating dictionaries 
+
+[print(l,j) for l,j in enumerate(example_dict)]					#we can also use enumerate for the dictionaries
+	
+new_dict = dict(enumerate(example))
+print(new_dict)
